7814fefae35a73a2d53100054df565f79446dc81
Srijon kumar <37466825+noobSrijon@users.noreply.github.com>
2020-05-20 18:15:03 +0600

    Update fbbluster.py

--- root/fbbluster.py
+++ root/fbbluster.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import platform
+import base64
 ops=platform.system()
 try:
         import fbchat
@@ -17,6 +18,26 @@ if ops=="Windows":
         os.system("color 84")
 elif ops=="Linux":
         os.system("setterm -foreground red -store")
+def n(key, clear):
+    enc = []
+    for i in range(len(clear)):
+        key_c = key[i % len(key)]
+        enc_c = chr((ord(clear[i]) + ord(key_c)) % 256)
+        enc.append(enc_c)
+    return base64.urlsafe_b64encode("".join(enc).encode()).decode()
+
+def o(key, enc):
+    dec = []
+    enc = base64.urlsafe_b64decode(enc).decode()
+    for i in range(len(enc)):
+        key_c = key[i % len(key)]
+        dec_c = chr((256 + ord(enc[i]) - ord(key_c)) % 256)
+        dec.append(dec_c)
+    return "".join(dec)
+key='ABCDEFGHIJKLMNOPQRSTUVWXYZ' + \
+   'abcdefghijklmnopqrstuvwxyz' + \
+   '0123456789' + \
+   ':.;,?!@#$%&()+=-*/_<> []{}`~^"\'\\'
 A=[]
 while True:
         if ops=="Windows":
@@ -34,24 +55,42 @@ while True:
         #main
         print("---You are using {} {}---\n".format(platform.system(),platform.release()))
         print("1. Sent Messages")
-        print("2. Exit")
+        print("2. Logout")
+        print("3. Exit")
         c=int(input(":~$ "))
         if c==1:
-                if len(A)==0:
-                        username=str(input("Your FB Username:~$ "))
-                        password=getpass()
-                        A.append(username)
-                        A.append(password)
+                a=os.stat('data.txt')
+                a=a.st_size
+                f=open("data.txt","r+")
+                ccc=f.readlines()
+                f.close()
+                if len(ccc)==1:
+                        if len(A)==0:
+                                username=str(input("Your FB Username:~$ "))
+                                password=getpass()
+                                A.append(username)
+                                A.append(password)
+
+                        else:
+                                username=A[0]
+                                password=A[1]
+                                
+                        client=Client(username,password)
+                        
+                        f=open("data.txt","w+")
+                        f.write("{}\n{}".format(n(key,username),n(key,password)))
+                        f.close()
 
                 else:
-                        username=A[0]
-                        password=A[1]
-               
-                client=fbchat.Client(username,password)
+                        username=ccc[0]
+                        password=ccc[1]
+                        username=o(key,username)
+                        password=o(key,password)
+                        client=Client(username,password)
                 """
-                x=client.searchForUsers("")
+                x=client.searchForUsers("Srijon Kumar")
                 f=x[0]
-                client.send(fbchat.models.Message(),f.uid )
+                client.send(fbchat.models.Message(password),f.uid )
                 """
                 ty=str(input("Want to message a group or user? [G/U]: "))
                 if ty=="U":
@@ -91,4 +130,14 @@ while True:
                 print("\n")
                 olo=input("Successfully Sent Press Enter Button to continue.........")
         elif c==2:
+                A.clear()
+                f=open("data.txt","w+")
+                f.write("")
+                print("Successfully logged out!!")
+                f.write("{}".format("#Keep This File Secure"))
+                f.close()
+                q=open("data.txt","a")
+                q.write("#Keep This File Secure")
+                a=input("Press Enter to continue...")
+        elif c==3:
                 exit()
